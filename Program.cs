791fa2c3a1a4bf9798abf0a8e9372687fb30134e
Didor Ada <didor.dada@gmail.com>
2021-10-14 15:58:29 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demander le nom de l'utilisateur
+            // Demander le numéro de téléphone de l'utilisateur
+
+            string nom = DemanderChaineUtilisateur("Quel est votre nom ?");
+
+            // 0610101010 -> 10 chiffres
+            string tel = DemanderChaineUtilisateur("Quel est votre numéro de téléphone ?");
+
+            Console.WriteLine();
+            Console.WriteLine("Bonjour " + nom + ", vous êtes joignable au " + tel);
+        }
+
+        static string DemanderChaineUtilisateur(string message)
+        {
+            Console.Write(message + " ");
+            string reponse = Console.ReadLine();
+            return reponse;
+        }
+
+    }
+}
